ec86f6e30cf3a17bae881833704a09bed9f6c748
zulldesign <admin@zulldesign.ml>
2015-01-15 11:03:08 +0800

    l

--- connect_to_mysql.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Isikan DB_host, UserName, Password, dan DB_Name bahagian ini
-mysql_connect("8bf4bda8-277a-43e6-94ef-a41d010fa8f7.mysql.sequelizer.com","bigpyalybjsqjkvh","cDn3bjsxiidMaLXtf44izKfNXxWWDz4sELQ26VB3aYCW6W8yFxHAERYyyGdLvD3Q") or die ("Could not connect.");
-mysql_select_db("db8bf4bda8277a43e694efa41d010fa8f7") or die ("no database");
-?>
